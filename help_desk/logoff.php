99570760a054ce7aded3d814c368894a549deacb
Iago Soares <66538620+IagoFSoares@users.noreply.github.com>
2021-10-04 11:22:49 -0300

    Add files via upload

---
+++ help_desk/logoff.php
@@ -0,0 +1,34 @@
+<?php 
+
+	/*
+	session_start();
+
+	echo '<pre>';
+	print_r($_SESSION);
+	echo '</pre>';
+
+ 	//remover indices do array de sessão
+	//unset()
+
+	unset($_SESSION['x']); // inteligência para remover o índice apenas se existir 
+
+	echo '<pre>';
+	print_r($_SESSION);
+	echo '</pre>';
+
+	//destruir a variavel de sessão
+	//session_destroy()
+
+	session_destroy(); // será destruída //forçar um redirecionamento
+
+	echo '<pre>';
+	print_r($_SESSION);
+	echo '</pre>';
+
+	*/
+
+	
+	session_destroy();
+	header('location: index.php');
+
+ ?>
